862a8be13c59457c3c5e33d802aca6e696427048
Hi. I'm Neo <jthug@naver.com>
2024-01-13 18:46:59 +0900

    init: 프로젝트 초기화

---
+++ module-data-sync/src/main/java/neo/chat/DataSyncApp.java
@@ -0,0 +1,13 @@
+package neo.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSyncApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSyncApp.class, args);
+    }
+
+}
