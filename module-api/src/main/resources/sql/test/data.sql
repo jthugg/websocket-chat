05ad84a54dc0c271a6f30d3f55f0d525d2435812
Hi. I'm Neo <jthug@naver.com>
2024-03-08 18:42:54 +0900

    chore: 테스트 데이터베이스 기본 데이터 입력
    테스트 환경에서 사용될 인메모리 디비 H2에 테스트 기동시 데이터 삽입
    스키마 입력, 테스트 데이터 입력

---
+++ module-api/src/main/resources/sql/test/data.sql
@@ -0,0 +1,7 @@
+-- define prod database data
+
+INSERT INTO `Member` VALUES (0, 'test00', 'test00', 'ROLE_NORMAL', now(), null);
+INSERT INTO `Member` VALUES (1, 'test01', 'test01', 'ROLE_NORMAL', now(), null);
+INSERT INTO `Member` VALUES (2, 'test02', 'test02', 'ROLE_NORMAL', now(), null);
+INSERT INTO `Member` VALUES (3, 'test03', 'test03', 'ROLE_NORMAL', now(), null);
+INSERT INTO `Member` VALUES (4, 'test04', 'test04', 'ROLE_NORMAL', now(), null);
