5aebf365dc1e9cc5c00909f1340ec079e6ff45bf
Hi. I'm Neo <jthug@naver.com>
2024-03-09 14:09:02 +0900

    feat: 회원 가입 기능 추가
    서비스 레이어 내 트랜잭션 분리로 트랜잭션 범위 최소화
    일부 서비스 메서드 자체가 트랜잭션 스크립트 역할을 하는경우 분리하지 않음

--- module-api/src/main/java/neo/chat/application/service/auth/service/MemberAuthService.java
+++ module-api/src/main/java/neo/chat/application/service/auth/service/MemberAuthService.java
@@ -1,7 +1,10 @@
 package neo.chat.application.service.auth.service;
 
+import neo.chat.application.service.auth.model.AuthResult;
+
 public interface MemberAuthService {
 
     boolean isUsernameAvailable(String username);
+    AuthResult register(String username, String password);
 
 }
--- module-api/src/main/java/neo/chat/application/service/auth/service/SimpleMemberAuthService.java
+++ module-api/src/main/java/neo/chat/application/service/auth/service/SimpleMemberAuthService.java
@@ -1,20 +1,49 @@
 package neo.chat.application.service.auth.service;
 
+import com.auth0.jwt.JWT;
 import lombok.RequiredArgsConstructor;
+import neo.chat.application.service.auth.model.AuthResult;
+import neo.chat.application.service.auth.properties.JWTProperties;
+import neo.chat.application.service.auth.tx.MemberAuthTransactionScript;
+import neo.chat.persistence.entity.member.Member;
 import neo.chat.persistence.repository.member.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @RequiredArgsConstructor
 public class SimpleMemberAuthService implements MemberAuthService {
 
     private final MemberRepository memberRepository;
+    private final MemberAuthTransactionScript transactionScript;
+    private final JWTProperties jwtProperties;
 
     @Override
     @Transactional(readOnly = true)
     public boolean isUsernameAvailable(String username) {
         return !memberRepository.existsByUsername(username);
+    }
+
+    @Override
+    public AuthResult register(String username, String password) {
+        Member member = transactionScript.createMember(username, password);
+        Instant now = Instant.now();
+        return new AuthResult(
+                member,
+                JWT.create()
+                        .withClaim(JWTProperties.USER_ID, member.getId())
+                        .withExpiresAt(now.plus(jwtProperties.atkTTL(), ChronoUnit.SECONDS))
+                        .sign(jwtProperties.algorithm()),
+                JWT.create()
+                        .withClaim(JWTProperties.USER_ID, member.getId())
+                        .withExpiresAt(now.plus(jwtProperties.rtkTTL(), ChronoUnit.SECONDS))
+                        .sign(jwtProperties.algorithm()),
+                jwtProperties.atkTTL(),
+                jwtProperties.rtkTTL()
+        );
     }
 
 }
