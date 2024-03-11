b78b465b0c078b657944a57da0eec2600892a847
Hi. I'm Neo <jthug@naver.com>
2024-03-11 22:16:44 +0900

    feat: 예외 처리
    회원 아이디 중복체크 예외 핸들러 추가

---
+++ module-api/src/main/java/neo/chat/presentation/auth/advice/MemberAuthControllerAdvice.java
@@ -0,0 +1,25 @@
+package neo.chat.presentation.auth.advice;
+
+import jakarta.validation.ConstraintViolationException;
+import neo.chat.application.service.exception.ApplicationException;
+import neo.chat.presentation.auth.controller.MemberAuthController;
+import neo.chat.presentation.util.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackageClasses = MemberAuthController.class)
+public class MemberAuthControllerAdvice {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<Response<String>> handleApplicationException(ApplicationException exception) {
+        return Response.responseEntityOf(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Response<String>> handleConstraintViolationException(ConstraintViolationException exception) {
+        return Response.responseEntityOf(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+}
