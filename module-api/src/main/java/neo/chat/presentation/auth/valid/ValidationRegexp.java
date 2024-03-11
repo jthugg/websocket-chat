423bdd9dbcae4d84ec6b3d50c5dcbaa0958b2b4b
Hi. I'm Neo <jthug@naver.com>
2024-03-11 22:14:49 +0900

    fix: 정규식 수정
    특수문자 이스케이프 처리

--- module-api/src/main/java/neo/chat/presentation/auth/valid/ValidationRegexp.java
+++ module-api/src/main/java/neo/chat/presentation/auth/valid/ValidationRegexp.java
@@ -2,6 +2,6 @@ package neo.chat.presentation.auth.valid;
 
 public class ValidationRegexp {
 
-    public static final String USERNAME = "^[a-zA-Z0-9.-_]{4,20}$";
+    public static final String USERNAME = "^[a-zA-Z0-9.\\-_]{4,20}$";
 
 }
