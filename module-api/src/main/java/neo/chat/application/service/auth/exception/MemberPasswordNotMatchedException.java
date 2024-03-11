50e507d42a918af2c0001df033907f66d4f1f889
Hi. I'm Neo <jthug@naver.com>
2024-03-11 23:27:02 +0900

    fix: 비밀번호 불일치 예외 클래스 상위 타입 변경

--- module-api/src/main/java/neo/chat/application/service/auth/exception/MemberPasswordNotMatchedException.java
+++ module-api/src/main/java/neo/chat/application/service/auth/exception/MemberPasswordNotMatchedException.java
@@ -1,6 +1,8 @@
 package neo.chat.application.service.auth.exception;
 
-public class MemberPasswordNotMatchedException extends RuntimeException {
+import neo.chat.application.service.exception.ApplicationException;
+
+public class MemberPasswordNotMatchedException extends ApplicationException {
 
     public static final String MESSAGE = "비밀번호가 일치하지 않습니다.";
 
