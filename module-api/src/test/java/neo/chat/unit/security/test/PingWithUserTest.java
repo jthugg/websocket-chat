5add0c8a588a633f80071cbf3a68d6b9053b466b
Hi. I'm Neo <jthug@naver.com>
2024-03-11 11:25:01 +0900

    chore: 테스트 프로파일 적용
    테스트 프로파일 적용
    기본 프로파일 local에 적용된 외부 데이터베이스 연결과 관계 없이 테스트 수행 할 수 있도록 변경
    test 프로파일에서는 H2 인메모리 데이터베이스에 엑세스

--- module-api/src/test/java/neo/chat/unit/security/test/PingWithUserTest.java
+++ module-api/src/test/java/neo/chat/unit/security/test/PingWithUserTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PingWithUserTest {
 
     @Autowired
