4b78f02884e0184bf6ca5bfa559f60a5db954078
Hi. I'm Neo <jthug@naver.com>
2024-03-12 16:02:31 +0900

    feat: 회원 탈퇴 엔드포인트 연결

--- module-api/src/main/java/neo/chat/presentation/auth/controller/MemberAuthController.java
+++ module-api/src/main/java/neo/chat/presentation/auth/controller/MemberAuthController.java
@@ -8,6 +8,7 @@ import neo.chat.application.service.auth.properties.JWTProperties;
 import neo.chat.application.service.auth.service.MemberAuthService;
 import neo.chat.presentation.auth.dto.request.LoginRequestDto;
 import neo.chat.presentation.auth.dto.request.RegisterRequestDto;
+import neo.chat.presentation.auth.dto.request.WithdrawRequestDto;
 import neo.chat.presentation.auth.dto.response.MemberAuthInfoDto;
 import neo.chat.presentation.auth.valid.ValidationMessage;
 import neo.chat.presentation.auth.valid.ValidationRegexp;
@@ -20,6 +21,7 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -81,16 +83,13 @@ public class MemberAuthController {
         // TODO: Expire Token
         // Expires token via blacklisting on Redis or some other data storage.
         // But do nothing in this version(v1.0.0) but just expire cookie value.
-        return Response.voidHeadedResponseEntityOf(
-                HttpStatus.OK,
-                headers -> headers.set(
-                        HttpHeaders.SET_COOKIE,
-                        ResponseCookie.from(JWTProperties.REFRESH_TOKEN)
-                                .maxAge(0L)
-                                .build()
-                                .toString()
-                )
-        );
+        return Response.voidHeadedResponseEntityOf(HttpStatus.OK, expireRefreshTokenCookie());
+    }
+
+    @DeleteMapping(ApiRoute.AUTH_WITHDRAW)
+    public ResponseEntity<Response<Void>> withdraw(@RequestBody @Valid WithdrawRequestDto dto) {
+        memberAuthService.withdraw(dto.password());
+        return Response.voidHeadedResponseEntityOf(HttpStatus.OK, expireRefreshTokenCookie());
     }
 
     private Consumer<HttpHeaders> tokenHeaderConsumer(AuthResult result) {
@@ -107,6 +106,16 @@ public class MemberAuthController {
                             .toString()
             );
         };
+    }
+
+    private Consumer<HttpHeaders> expireRefreshTokenCookie() {
+        return headers -> headers.set(
+                HttpHeaders.SET_COOKIE,
+                ResponseCookie.from(JWTProperties.REFRESH_TOKEN)
+                        .maxAge(0L)
+                        .build()
+                        .toString()
+        );
     }
 
 }
