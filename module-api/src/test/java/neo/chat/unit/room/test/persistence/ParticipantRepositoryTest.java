6b345a968769706c951177c83b5de2706f8e12be
Hi. I'm Neo <jthug@naver.com>
2024-03-22 18:40:38 +0900

    test: 참여중인 채팅방 조회 테스트
    테스트 데이터 입력
    조회 쿼리 및 커서 기반 페이징 테스트

---
+++ module-api/src/test/java/neo/chat/unit/room/test/persistence/ParticipantRepositoryTest.java
@@ -0,0 +1,82 @@
+package neo.chat.unit.room.test.persistence;
+
+import lombok.extern.slf4j.Slf4j;
+import neo.chat.application.service.room.model.ChatRoomSortOption;
+import neo.chat.persistence.entity.member.Member;
+import neo.chat.persistence.repository.member.MemberRepository;
+import neo.chat.persistence.repository.participant.ParticipantRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@Slf4j
+@ActiveProfiles("test")
+@DataJpaTest(showSql = false)
+@Sql(scripts = {"classpath:sql/test/schema.sql", "classpath:sql/test/data.sql"})
+public class ParticipantRepositoryTest {
+
+    @Autowired
+    ParticipantRepository participantRepository;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testCase00() {
+        Member member = memberRepository.findById(0L).get();
+        participantRepository.findByMemberAndRemovedAtIsNull(
+                member,
+                PageRequest.of(
+                        0,
+                        3,
+                        Sort.Direction.DESC,
+                        ChatRoomSortOption.DEFAULT.getFieldName()
+                )
+        ).forEach(value -> log.info("""
+                
+                participant id: {}
+                room id: {}
+                """, value.getId(), value.getRoom().getId()));
+    }
+
+    @Test
+    void testCase01() {
+        Member member = memberRepository.findById(0L).get();
+        participantRepository.findByMemberAndRemovedAtIsNull(
+                member,
+                PageRequest.of(
+                        0,
+                        6,
+                        Sort.Direction.DESC,
+                        ChatRoomSortOption.DEFAULT.getFieldName()
+                )
+        ).forEach(value -> log.info("""
+                
+                participant id: {}
+                room id: {}
+                """, value.getId(), value.getRoom().getId()));
+    }
+
+    @Test
+    void testCase02() {
+        Member member = memberRepository.findById(0L).get();
+        participantRepository.findByMemberAndIdLessThanAndRemovedAtIsNull(
+                member,
+                28L,
+                PageRequest.of(
+                        0,
+                        3,
+                        Sort.Direction.DESC,
+                        ChatRoomSortOption.DEFAULT.getFieldName()
+                )
+        ).forEach(value -> log.info("""
+                
+                participant id: {}
+                room id: {}
+                """, value.getId(), value.getRoom().getId()));
+    }
+
+}
