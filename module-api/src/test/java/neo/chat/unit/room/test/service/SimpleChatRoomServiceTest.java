1bc29b4283569c842ea002de9be2ee82864c8b42
Hi. I'm Neo <jthug@naver.com>
2024-03-27 02:01:49 +0900

    test: 채팅 방 입장 추가 테스트
    서비스 레이어 단위 테스트

--- module-api/src/test/java/neo/chat/unit/room/test/service/SimpleChatRoomServiceTest.java
+++ module-api/src/test/java/neo/chat/unit/room/test/service/SimpleChatRoomServiceTest.java
@@ -1,10 +1,18 @@
 package neo.chat.unit.room.test.service;
 
+import neo.chat.application.service.room.exception.AlreadyEnteredRoomException;
+import neo.chat.application.service.room.exception.ChatRoomHasNoVacancyException;
+import neo.chat.application.service.room.exception.ChatRoomPasswordNotMatchedException;
+import neo.chat.application.service.room.model.EnterChatRoomRequest;
 import neo.chat.application.service.room.model.OpenChatRoomRequest;
 import neo.chat.application.service.room.service.SimpleChatRoomService;
 import neo.chat.application.service.room.tx.ChatRoomTransactionScript;
 import neo.chat.persistence.entity.member.Member;
+import neo.chat.persistence.entity.room.Room;
+import neo.chat.persistence.repository.participant.ParticipantRepository;
+import neo.chat.persistence.repository.room.RoomRepository;
 import neo.chat.settings.context.AuthMemberContextHolder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,8 +20,11 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("채팅방 서비스 레이어 테스트")
@@ -21,8 +32,12 @@ public class SimpleChatRoomServiceTest {
 
     @Mock
     ChatRoomTransactionScript chatRoomTransactionScript;
-    @Mock
+    @Spy
     PasswordEncoder passwordEncoder;
+    @Mock
+    ParticipantRepository participantRepository;
+    @Mock
+    RoomRepository roomRepository;
     @InjectMocks
     SimpleChatRoomService simpleChatRoomService;
 
@@ -41,6 +56,94 @@ public class SimpleChatRoomServiceTest {
         Mockito.when(chatRoomTransactionScript.createRoom(ArgumentMatchers.any())).thenReturn(null);
 
         simpleChatRoomService.openChatRoom(request);
+    }
+
+    @Test
+    @DisplayName("채팅 방 입장 테스트: 성공 케이스")
+    void enterTestCase01() {
+        Member member = new Member(100L, "test", "test");
+        EnterChatRoomRequest request = new EnterChatRoomRequest(12L, "iam00", null);
+        Room room = Room.builder()
+                .id(12L)
+                .title("testTitle12")
+                .capacity(50)
+                .build();
+
+        AuthMemberContextHolder.set(member);
+        Mockito.when(participantRepository.existsByMemberAndRoomIdAndRemovedAtIsNull(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+        )).thenReturn(false);
+        Mockito.when(roomRepository.findByIdJoinFetchParticipantsWithLock(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(room));
+
+        Assertions.assertDoesNotThrow(() -> simpleChatRoomService.enterRoom(request));
+    }
+
+    @Test
+    @DisplayName("채팅 방 입장 테스트: 실패 케이스 - 최대 인원 도달")
+    void enterTestCase02() {
+        Member member = new Member(100L, "test", "test");
+        EnterChatRoomRequest request = new EnterChatRoomRequest(12L, "iam00", null);
+        Room room = Room.builder()
+                .id(12L)
+                .title("testTitle12")
+                .capacity(1)
+                .build();
+
+        AuthMemberContextHolder.set(member);
+        Mockito.when(participantRepository.existsByMemberAndRoomIdAndRemovedAtIsNull(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+        )).thenReturn(false);
+        Mockito.when(roomRepository.findByIdJoinFetchParticipantsWithLock(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(room));
+
+        Assertions.assertThrows(ChatRoomHasNoVacancyException.class, () -> simpleChatRoomService.enterRoom(request));
+    }
+
+    @Test
+    @DisplayName("채팅 방 입장 테스트: 실패 케이스 - 비밀번호 불일치")
+    void enterTestCase03() {
+        Member member = new Member(100L, "test", "test");
+        EnterChatRoomRequest request = new EnterChatRoomRequest(12L, "iam00", null);
+        Room room = Room.builder()
+                .id(12L)
+                .title("testTitle12")
+                .capacity(50)
+                .password("test")
+                .build();
+
+        AuthMemberContextHolder.set(member);
+        Mockito.when(participantRepository.existsByMemberAndRoomIdAndRemovedAtIsNull(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+        )).thenReturn(false);
+        Mockito.when(roomRepository.findByIdJoinFetchParticipantsWithLock(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(room));
+
+        Assertions.assertThrows(
+                ChatRoomPasswordNotMatchedException.class,
+                () -> simpleChatRoomService.enterRoom(request)
+        );
+    }
+
+    @Test
+    @DisplayName("채팅 방 입장 테스트: 실패 케이스 - 이미 참여중인 방")
+    void enterTestCase04() {
+        Member member = new Member(100L, "test", "test");
+        EnterChatRoomRequest request = new EnterChatRoomRequest(12L, "iam00", null);
+
+        AuthMemberContextHolder.set(member);
+        Mockito.when(participantRepository.existsByMemberAndRoomIdAndRemovedAtIsNull(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+        )).thenReturn(true);
+
+        Assertions.assertThrows(
+                AlreadyEnteredRoomException.class,
+                () -> simpleChatRoomService.enterRoom(request)
+        );
     }
 
 }
